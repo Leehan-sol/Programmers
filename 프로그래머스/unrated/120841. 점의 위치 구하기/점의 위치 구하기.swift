05809f759d4debf277a32b66323610e450eb3867
Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
2024-01-09 12:45:19 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.swift
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 func solution(_ dot:[Int]) -> Int {
-      guard dot.count > 1 else { return 0 }
     var x:Int = dot[0]
     var y:Int = dot[1]
     return x < 0 && y < 0 ? 3 : x < 0 ? 2 : y > 0 ? 1 : 4
