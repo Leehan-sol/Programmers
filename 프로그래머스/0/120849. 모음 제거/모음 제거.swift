bfb03c2091d345f50f6e24dba59a0353372ed6e3
Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
2024-03-15 21:33:17 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.swift
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
-    let a = my_string.replacingOccurrences(of: "a", with: "")
-    let e = a.replacingOccurrences(of: "e", with: "")
-    let i = e.replacingOccurrences(of: "i", with: "")
-    let o = i.replacingOccurrences(of: "o", with: "")
-    let u = o.replacingOccurrences(of: "u", with: "")
-    
-    return u
+    return my_string.filter { !["a", "e", "i", "o", "u"].contains($0)}
 }
