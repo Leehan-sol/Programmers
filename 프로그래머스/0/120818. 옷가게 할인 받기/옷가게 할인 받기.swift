1672330c00625c1c09c860003498109bace17442
Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
2024-03-15 18:04:50 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
@@ -1,7 +1,7 @@
 import Foundation
 func solution(_ price:Int) -> Int {
     switch price {
-        case 100000..<300000:
+        case 100000..<300000: 
         return Int(Double(price) * 0.95)
         case 300000..<500000:
         return Int(Double(price) * 0.9)
@@ -9,5 +9,5 @@ func solution(_ price:Int) -> Int {
         return Int(Double(price) * 0.8)
         default:
         return price
-}
+    }
 }
