1b6eb6de1e28f9bb06b3e035024460ba961460cd
Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
2024-03-16 16:12:07 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.swift
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.swift
@@ -2,17 +2,10 @@ import Foundation
 
 func solution(_ hp:Int) -> Int {
     var result = 0
-    let a = hp % 5 
     
-    if a > 0 {
-        result += hp / 5 
-        result += a / 3
-        let b = a % 3
-        if b > 0 {
-            result += b 
-        }
-    } else {
-        result += hp / 5
-    }
+    result += hp / 5
+    result += (hp % 5) / 3
+    result += (hp % 5) % 3
+    
     return result
 }
