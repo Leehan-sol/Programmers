20e1d841dc6b4fcc50891b3480433ff7f4a880d4
Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
2024-03-18 16:32:20 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.swift
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.swift
@@ -1,12 +1,15 @@
 import Foundation
 
 func solution(_ num_list: [Int], _ n: Int) -> [[Int]] {   
-    var result = [[Int]]()
-
-    for i in stride(from: 0, to: num_list.count, by: n) {
-        let endIndex = min(i + n, num_list.count)
-        result.append(Array(num_list[i..<endIndex]))
-    }
+    var a = [Int]()
+    var result: [[Int]] = []
     
+    for i in num_list {
+        a.append(i) 
+        if a.count == n {
+            result.append(a)
+            a = []
+        }
+    }
     return result
 }
