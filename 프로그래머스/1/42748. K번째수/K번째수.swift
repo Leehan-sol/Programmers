8fd463378b50efdcdb8eee3679aeb2016a9f2219
Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
2024-03-21 18:06:13 +0900

    [level 1] Title: K번째수, Time: 0.19 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -1,18 +1,18 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    var sortedArray = [Int]()
     var result = [Int]()
     
-    for r in 0..<commands.count {
-        var i = commands[r][0] - 1
-        var j = commands[r][1] - 1
-        var k = commands[r][2] - 1  
+    for com in commands {
+        var i = com[0] - 1
+        var j = com[1] - 1
+        var k = com[2] - 1
         
-        sortedArray = array[i...j].sorted()
-        
-        result.append(sortedArray[k])  
+        let arr = array[i...j].sorted()
+        print(arr)
+        result.append(arr[k])
     }
     
     return result
+    
 }
