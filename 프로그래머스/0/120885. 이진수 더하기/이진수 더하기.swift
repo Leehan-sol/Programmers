dbe2198254c9e7080bab730536087c286c401c3a
Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
2024-05-27 17:17:01 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.swift
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.swift
@@ -1,10 +1,8 @@
 import Foundation
 
 func solution(_ bin1: String, _ bin2: String) -> String {
-    let num1 = Int(bin1, radix: 2) ?? 0
-    let num2 = Int(bin2, radix: 2) ?? 0
+    let num1 = Int(bin1, radix: 2)!
+    let num2 = Int(bin2, radix: 2)!
     
-    let sum = num1 + num2
-    
-    return String(sum, radix: 2)
+    return String(num1 + num2, radix: 2)
 }
