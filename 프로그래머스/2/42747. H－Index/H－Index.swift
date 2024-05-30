79decadc40d4da84e70b6de033b8adac8bbeb15d
Leehan-sol <139109343+Leehan-sol@users.noreply.github.com>
2024-05-30 16:26:55 +0900

    [level 2] Title: H-Index, Time: 0.10 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.swift
+++ 프로그래머스/2/42747. H－Index/H－Index.swift
@@ -1,18 +1,17 @@
 import Foundation
 
 func solution(_ citations:[Int]) -> Int {
-    var array = citations.sorted()
+    var sortedArr = citations.sorted()
     var count = citations.count
-    var result = 0
-
-    for i in array {
+    var result = 0 
+    
+    for i in sortedArr {
         if i >= count {
             result = count
         } else {
             count -= 1
         }
     }
-    
     return result
 }
 
